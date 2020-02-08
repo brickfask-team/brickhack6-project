f78309d40423869789877030df65e9bf20afb8b7
Francisco Cornejo <franciscornejo.garcia@gmail.com>
2020-02-08 15:10:23 -0500

    Add start script

---
+++ app.js
@@ -0,0 +1,25 @@
+// Packages
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const express = require('express');
+const app = express();
+
+// Models
+
+
+
+// Routes
+
+
+// App Setup
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+// Express Setup
+app.use(express.static('public'));
+
+// Server Connection
+app.listen(3000, () => {
+  console.log('Server online...');
+});
