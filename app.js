4905409754e1a2e02640711b9f119704aeb8065b
Francisco Cornejo <franciscornejo.garcia@gmail.com>
2020-02-09 04:06:04 -0500

    add backend

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const methodOverride = require('method-override');
 const express = require('express');
 const app = express();
 
-// Routes
-const indexRoutes = require('./routes/index');
-app.use(indexRoutes);
-
 // App Setup
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 // Express Setup
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
+
+// Routes
+const indexRoutes = require('./routes/index');
+app.use(indexRoutes);
 
 // Server Connection
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 // Packages
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+   res.render('index');
 });
 
 module.exports = router;
